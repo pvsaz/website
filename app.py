6eaa3b6279a072750b25953c4a45e4f1e081b808
Purna Shah <pvs25@cornell.edu>
2022-10-14 02:38:58 -0700

    testing portability improvement

--- app.py
+++ app.py
@@ -48,4 +48,6 @@ app.register_blueprint(blog_views.blog, url_prefix="")
 
 # run app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run()
+
+#host="0.0.0.0", port=int(os.environ.get("PORT", 5000))
